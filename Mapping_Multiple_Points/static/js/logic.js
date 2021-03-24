081cb622e80e37114c7bd829c2760650ab9a9595
Farhad Kerimov <farhad.kerimov@gmail.com>
2021-03-24 14:18:21 -0400

    Adding script with circle markers proportionate to population

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,8 +1,10 @@
 // Add console.log to check if the code is working.
 console.log("working")
 
+let cityData = cities;
+
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0687, -118.3228], 14); 
+let map = L.map('mapid').setView([39.8283, -98.5795], 4); 
 
 // //Alternatively: 
 // let map = L.map("mapid", {
@@ -10,14 +12,17 @@ let map = L.map('mapid').setView([34.0687, -118.3228], 14);
 //     zoom:4
 // })
 
-// Add a marker to the map for LA, CA
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-// Add circle
-L.circleMarker([34.0687, -118.3228], {
-    radius: 300,
-    color: 'black',
-    fillColor: '#ffffa1'
-}).addTo(map);
+  // Loop through the cities array and create one marker for each city
+  cityData.forEach(city => {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        weight: 4,
+        color: 'orange'
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3> Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 // Create tile layer for the background of the map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -26,12 +31,6 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     id: 'mapbox/dark-v10',
     accessToken: API_KEY
 });
-    // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //     maxZoom: 18,
-    //     id: 'dark-v10',
-    //     tileSize: 512,
-    //     accessToken: API_KEY
-    // });
+
 // Add the 'graymap' tile layer to the map.
 streets.addTo(map);
