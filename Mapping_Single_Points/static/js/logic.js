6961a0fbe95871c7b9b132d05712217e6967e6d1
Farhad Kerimov <farhad.kerimov@gmail.com>
2021-03-24 12:24:47 -0400

    Adding script with dark tile layer and circleMarker

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working")
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4); 
+let map = L.map('mapid').setView([34.0687, -118.3228], 14); 
 
 // //Alternatively: 
 // let map = L.map("mapid", {
@@ -10,12 +10,28 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom:4
 // })
 
+// Add a marker to the map for LA, CA
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Add circle
+L.circleMarker([34.0687, -118.3228], {
+    radius: 300,
+    color: 'black',
+    fillColor: '#ffffa1'
+}).addTo(map);
+
 // Create tile layer for the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    id: 'mapbox/dark-v10',
     accessToken: API_KEY
 });
-
+    // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    //     maxZoom: 18,
+    //     id: 'dark-v10',
+    //     tileSize: 512,
+    //     accessToken: API_KEY
+    // });
 // Add the 'graymap' tile layer to the map.
 streets.addTo(map);
